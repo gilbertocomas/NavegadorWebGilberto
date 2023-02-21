d0982241da28d949d16bd91c97bee172525d86ab
= <=>
2023-02-21 00:01:49 -0400

    Agregar archivos de proyecto.

---
+++ NavegadorWebGilberto/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace NavegadorWebGilberto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnSiguiente = new System.Windows.Forms.Button();
+            this.btnAtras = new System.Windows.Forms.Button();
+            this.btnRecargar = new System.Windows.Forms.Button();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.txtURL = new System.Windows.Forms.ComboBox();
+            this.btnAlto = new System.Windows.Forms.Button();
+            this.btnIr = new System.Windows.Forms.Button();
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSiguiente
+            // 
+            this.btnSiguiente.Image = ((System.Drawing.Image)(resources.GetObject("btnSiguiente.Image")));
+            this.btnSiguiente.Location = new System.Drawing.Point(73, 10);
+            this.btnSiguiente.Name = "btnSiguiente";
+            this.btnSiguiente.Size = new System.Drawing.Size(55, 28);
+            this.btnSiguiente.TabIndex = 1;
+            this.btnSiguiente.UseVisualStyleBackColor = true;
+            this.btnSiguiente.Click += new System.EventHandler(this.btnSiguiente_Click);
+            // 
+            // btnAtras
+            // 
+            this.btnAtras.Image = ((System.Drawing.Image)(resources.GetObject("btnAtras.Image")));
+            this.btnAtras.Location = new System.Drawing.Point(12, 10);
+            this.btnAtras.Name = "btnAtras";
+            this.btnAtras.Size = new System.Drawing.Size(55, 28);
+            this.btnAtras.TabIndex = 1;
+            this.btnAtras.UseVisualStyleBackColor = true;
+            this.btnAtras.Click += new System.EventHandler(this.btnAtras_Click);
+            // 
+            // btnRecargar
+            // 
+            this.btnRecargar.Image = ((System.Drawing.Image)(resources.GetObject("btnRecargar.Image")));
+            this.btnRecargar.Location = new System.Drawing.Point(134, 10);
+            this.btnRecargar.Name = "btnRecargar";
+            this.btnRecargar.Size = new System.Drawing.Size(55, 28);
+            this.btnRecargar.TabIndex = 1;
+            this.btnRecargar.UseVisualStyleBackColor = true;
+            this.btnRecargar.Click += new System.EventHandler(this.btnRecargar_Click);
+            // 
+            // btnHome
+            // 
+            this.btnHome.Image = ((System.Drawing.Image)(resources.GetObject("btnHome.Image")));
+            this.btnHome.Location = new System.Drawing.Point(256, 10);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(55, 28);
+            this.btnHome.TabIndex = 1;
+            this.btnHome.UseVisualStyleBackColor = true;
+            this.btnHome.Click += new System.EventHandler(this.btnHome_Click_1);
+            // 
+            // txtURL
+            // 
+            this.txtURL.FormattingEnabled = true;
+            this.txtURL.Location = new System.Drawing.Point(317, 12);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(477, 24);
+            this.txtURL.TabIndex = 2;
+            // 
+            // btnAlto
+            // 
+            this.btnAlto.Image = ((System.Drawing.Image)(resources.GetObject("btnAlto.Image")));
+            this.btnAlto.Location = new System.Drawing.Point(195, 9);
+            this.btnAlto.Name = "btnAlto";
+            this.btnAlto.Size = new System.Drawing.Size(55, 28);
+            this.btnAlto.TabIndex = 1;
+            this.btnAlto.UseVisualStyleBackColor = true;
+            this.btnAlto.Click += new System.EventHandler(this.btnAlto_Click);
+            // 
+            // btnIr
+            // 
+            this.btnIr.Image = ((System.Drawing.Image)(resources.GetObject("btnIr.Image")));
+            this.btnIr.Location = new System.Drawing.Point(802, 12);
+            this.btnIr.Name = "btnIr";
+            this.btnIr.Size = new System.Drawing.Size(55, 28);
+            this.btnIr.TabIndex = 1;
+            this.btnIr.UseVisualStyleBackColor = true;
+            this.btnIr.Click += new System.EventHandler(this.btnIr_Click);
+            // 
+            // webView21
+            // 
+            this.webView21.AllowExternalDrop = true;
+            this.webView21.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Location = new System.Drawing.Point(12, 44);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(1034, 538);
+            this.webView21.TabIndex = 3;
+            this.webView21.ZoomFactor = 1D;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1051, 611);
+            this.Controls.Add(this.webView21);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.btnAtras);
+            this.Controls.Add(this.btnIr);
+            this.Controls.Add(this.btnAlto);
+            this.Controls.Add(this.btnHome);
+            this.Controls.Add(this.btnRecargar);
+            this.Controls.Add(this.btnSiguiente);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Gilberto Comas 2022-0700";
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnSiguiente;
+        private System.Windows.Forms.Button btnAtras;
+        private System.Windows.Forms.Button btnRecargar;
+        private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.ComboBox txtURL;
+        private System.Windows.Forms.Button btnAlto;
+        private System.Windows.Forms.Button btnIr;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+    }
+}
+
---
+++ NavegadorWebGilberto/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace NavegadorWebGilberto
+{
+    public partial class Form1 : Form
+    {
+      
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeAsync();
+        }
+
+        private async void InitializeAsync()
+        {
+            await webView21.EnsureCoreWebView2Async();
+        }
+        
+        private void btnIr_Click(object sender, EventArgs e)
+        {
+            //Boton para navegar a la URL indicada en el TextBox
+            String direccion = txtURL.Text;
+            if (!direccion.Contains("https://"))
+            {
+                direccion = "https://" + direccion;
+            }
+            webView21.CoreWebView2.Navigate(direccion);
+               
+        }
+
+        private void btnAtras_Click(object sender, EventArgs e)
+        {
+            webView21.CoreWebView2.GoBack();
+        }
+
+        private void btnSiguiente_Click(object sender, EventArgs e)
+        {
+            webView21.CoreWebView2.GoForward();
+        }
+
+        private void btnRecargar_Click(object sender, EventArgs e)
+        {
+            webView21.CoreWebView2.Reload();
+        }
+
+        private void btnAlto_Click(object sender, EventArgs e)
+        {
+            webView21.CoreWebView2.Stop();
+        }
+
+        private void btnHome_Click_1(object sender, EventArgs e)
+        {
+            // Reemplaza la URL de ejemplo con la página de inicio que deseas cargar
+            String direccion = "https://www.google.com";
+            txtURL.Text = direccion;
+            webView21.CoreWebView2.Navigate(txtURL.Text);
+        }
+    }
+}
